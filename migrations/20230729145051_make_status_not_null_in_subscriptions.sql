a3c31b7f5c7ae9d2d65a0d765233c431ba5b7c40
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2023-07-30 00:43:55 +0900

    Run migrate bash command each time set up new database migration. - Need to run bash script to migrate database. - Once a migration script applied to a database, that script is not allowed to modify, if modified, fail to apply to that database.

--- migrations/20230729145051_make_status_not_null_in_subscriptions.sql
+++ migrations/20230729145051_make_status_not_null_in_subscriptions.sql
@@ -3,7 +3,7 @@ BEGIN;
     -- Loop and backfill all empty values on `status` column to `confirmed`
     UPDATE subscriptions
         SET status = 'confirmed'
-        WHERE status IS NULL
+        WHERE status IS NULL;
     -- Change type of values in `status` column to mandatory aka `not null`
     ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
 COMMIT;
